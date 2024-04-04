7645019fe811af71590f48e2ca43446274fbb1fa
julesDRS <jules.de-rus@epitech.eu>
2024-04-04 11:34:42 +0200

    add main

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
     printf("i don't want World !\n");
-    int nooooo = 69;
-    printf("Goodbye World !\n");
+    int nooooo = 420;
+    printf("Good World !\n");
     return 0;
 }
